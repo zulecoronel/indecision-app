93a6bfbe79374d860597dd1baff2e10eb8cb3ca1
zulemaCoronel <zulema.cl.09@gmail.com>
2024-08-19 15:39:55 -0600

    Pruebas sobre sumatoria de arreglo

--- tests/helpers/sum.test.ts
+++ tests/helpers/sum.test.ts
@@ -1,16 +1,36 @@
 // sum.test.js
 import { expect, test } from 'vitest';
-import { sum } from '../../src/helpers/sum';
+import { addArray, sum } from '../../src/helpers/sum';
+import { describe } from 'node:test';
 
 test('adds 1 + 2 to equal 3', () => {
 
-//Preparacion
-const a = 1;
-const b = 2;
+    //Preparacion
+    const a = 1;
+    const b = 2;
+    
+    //Estimulo
+    const result = sum (a,b);
+    
+    //El comportamiento esperado
+    expect(result).toBe(a + b)
+    });
+    
+describe('addArray function',()=> {
+test ('should return 0 if the array is empty' , () => {
+    const numberArray = [];
 
-//Estimuloo
-const result = sum (a,b);
+    const result = addArray(numberArray);
+  
+    expect(result).toBe(0);
+});
 
-//El comportamiento esperado
-expect(result).toBe(a + b)
-})
+test ('should return the proper value of the addArray function' , () => {
+    const numberArray = [1,2,3,4,5];
+
+    const result = addArray(numberArray);
+  
+    expect(result).toBe(15);
+});
+
+});
